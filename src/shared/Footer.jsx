55b749006e36c2f07251a7005732f928560ed065
brainstromsurfer <wishmeheaven@gmail.com>
2023-10-05 19:37:40 +0300

    Setting the current year (dynamically)

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -2,10 +2,11 @@ import SocialLink from "../components/SocialLink"
 import { socialLinks } from "../utils/website_data"
 
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
     <footer className="main-footer">
   <div className="footer-content container">
-    <p>Copyright © 2019. All Rights Reserved</p>
+    <p>Copyright © {year}. All Rights Reserved</p>
     <div className="social">
       {socialLinks.map(({id, icon, awesomeClass}) => <SocialLink key={id} icon={icon} />)}
     </div>
