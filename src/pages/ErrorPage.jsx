6c89ff1b5f6442efc517e94fbb81a5d96fda69bf
brainstromsurfer <wishmeheaven@gmail.com>
2023-10-05 05:43:12 +0300

    Some css vars and final polish

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,17 +1,29 @@
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-      <Link to="/" className="not-found-home-link">Return Home</Link>
-    </div>
+    <section className="page_404">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 ">
+            <div className="col-sm-10 col-sm-offset-1  text-center">
+              <div className="four_zero_four_bg">
+                <h1 className="text-center ">404</h1>
+              </div>
+              <div className="content_box_404">
+                <h3>Look like you're lost</h3>
+                <p>the page you are looking for not available!</p>
+                <button className="link_404">
+                  <Link to="/">
+                    Go Home
+                  </Link>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
